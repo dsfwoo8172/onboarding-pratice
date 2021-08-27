e54f60abb891433b270e2ae69d164dec60006634
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-08-27 18:41:35 +0800

    feat(user): add validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, presence: true # 不能為空
+  validates :gender, inclusion: { in: %w(male female others), # 必須是這三個值
+                                message: 'Please fill one of options in. EX: male female others' }
+  validates :age, numericality: { only_integer: true, # 只能為整數且必須大於 0
+                                  greater_than_or_equal_to: 0}
 end
