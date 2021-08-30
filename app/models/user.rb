09cc6828faed56b2d00d33f58c106d9270650416
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-08-30 13:19:06 +0800

    feat(user): change code style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, presence: true # 不能為空
-  validates :gender, inclusion: { in: %w(male female others), # 必須是這三個值
-                                message: 'Please fill one of options in. EX: male female others' }
-  validates :age, numericality: { only_integer: true, # 只能為整數且必須大於 0
+  GENDER = %w[male female others]
+  validates :first_name, :last_name, presence: true
+  validates :gender, inclusion: { in: GENDER,
+                                  message: "Please fill one of options in. EX: #{GENDER}.join(' ')" }
+  validates :age, numericality: { only_integer: true,
                                   greater_than_or_equal_to: 0}
 end
