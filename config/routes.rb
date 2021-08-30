098c8625732b0e0f657c965c6bcc6636a6040d05
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-08-30 13:23:16 +0800

    feat(route): takeout users#count

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users do
     collection do
-      get :count, to: "users#count" # get total number of users
+      get :count
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
