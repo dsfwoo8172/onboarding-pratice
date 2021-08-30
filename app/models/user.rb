be4af5cc8386d340572461868298e4768217924f
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-08-30 15:14:42 +0800

    feat(db): change database to mongodb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class User
+
+  include Mongoid::Document
+
   GENDER = %w[male female others]
+
+  field :first_name, type: String
+  field :last_name, type: String
+  field :gender, type: String
+  field :age, type: Integer
+
   validates :first_name, :last_name, presence: true
   validates :gender, inclusion: { in: GENDER,
                                   message: "Please fill one of options in. EX: #{GENDER}.join(' ')" }
--- db/migrate/20210827103211_add_age_and_gender_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddAgeAndGenderToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :age, :integer
-    add_column :users, :gender, :string
-  end
-end
