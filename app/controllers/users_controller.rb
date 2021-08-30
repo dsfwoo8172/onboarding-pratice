0a3b3439d121217ff943ccfc184b56991ed7f27f
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-08-30 18:58:33 +0800

    feat(user): take out byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,6 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        byebug
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
