d7c136473cbf881ec1f81932c116abb383d9216c
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-09-01 18:19:01 +0800

    feat(angular): add user edit controller and update user by ajax and front-end validation

---
+++ app/assets/javascripts/angular/controller.users.edit.js
@@ -0,0 +1,27 @@
+var app = angular.module('UsersEditApp', []);
+
+app.controller('UsersEditController', function($scope, $http, user_config) {
+  $scope.config = user_config
+  let userId = location.href.split('/')[4]
+
+  let getUser = () => {
+    $http.get(`/users/${userId}/edit.json`).then(res => {
+      $scope.user = res.data.user
+    }, err => console.log(err.message)) 
+  }
+  
+  getUser()
+})
+
+app.controller('SubmitController', function ($scope, $http) {
+  let userId = location.href.split('/')[4]
+  let token = document.querySelector('[name="csrf-token"]').content
+  
+  $scope.submit = () => {
+    $http.defaults.headers.common['X-CSRF-TOKEN'] = token
+    $http.patch(`/users/${userId}.json`, { user: $scope.user } ).then(res => {
+      location.replace(res.data.url.replace('.json', ''))
+      alert('successfully')
+    })
+  }
+})
