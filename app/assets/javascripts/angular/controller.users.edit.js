0827730d21e4591dc5004ba428158cd80e690637
Jerry.Fang <jerry.fang@shoplineapp.com>
2021-09-02 10:30:13 +0800

    feat(angular): change validation way of written

--- app/assets/javascripts/angular/controller.users.edit.js
+++ app/assets/javascripts/angular/controller.users.edit.js
@@ -3,22 +3,18 @@ var app = angular.module('UsersEditApp', []);
 app.controller('UsersEditController', function($scope, $http, user_config) {
   $scope.config = user_config
   $scope.inteValidation = /^\d+$/
-  let userId = location.href.split('/')[4]
+  let userId = user_config.user_id
 
   let getUser = () => {
     $http.get(`/users/${userId}/edit.json`).then(res => {
       $scope.user = res.data.user
     }, err => console.log(err.message)) 
   }
-  
-  getUser()
-})
 
-app.controller('SubmitController', function ($scope, $http) {
-  let userId = location.href.split('/')[4]
-  let token = document.querySelector('[name="csrf-token"]').content
-  
+  getUser()
+
   $scope.submit = () => {
+    let token = document.querySelector('[name="csrf-token"]').content
     $http.defaults.headers.common['X-CSRF-TOKEN'] = token
     $http.patch(`/users/${userId}.json`, { user: $scope.user } ).then(res => {
       location.replace(res.data.url.replace('.json', ''))
